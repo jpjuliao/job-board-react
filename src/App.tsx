a96a507966e85ddbb81ffda42a7d12c019558642
JP Juliao <jpjuliao@gmail.com>
2025-04-14 16:27:58 -0500

    Install Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='m-5'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
