c1dfea2b1e93b6536e4ee306abd3686aec0a5915
JP Juliao <jpjuliao@gmail.com>
2025-04-22 11:54:37 -0500

    Add support to children components to withProfiler component.

--- src/components/withProfiler.tsx
+++ src/components/withProfiler.tsx
@@ -1,25 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
-/**
- * A higher-order component that wraps a component and logs the time it takes to render to the console.
- *
- * The component uses the `useEffect` hook to measure the time it takes to render the wrapped component.
- * The render time is logged to the console as soon as the component is rendered.
- *
- * @param {React.ComponentType} WrappedComponent - The component to wrap.
- * @returns {React.FC<React.PropsWithChildren<unknown>>} - The wrapped component.
- */
-const withProfiler = (WrappedComponent: React.ComponentType) => {
+const withProfiler = (WrappedComponent: React.ComponentType<React.PropsWithChildren<unknown>>) => {
 
-  /**
-   * Component that wraps a component and logs the time it takes to render to the console.
-   *
-   * The component uses the `useEffect` hook to measure the time it takes to render the wrapped component.
-   * The render time is logged to the console as soon as the component is rendered.
-   *
-   * @param {React.PropsWithChildren<unknown>} props - Props to pass to the wrapped component.
-   * @returns {React.ReactElement} The wrapped component.
-   */
   const ProfilerComponent: React.FC<React.PropsWithChildren<unknown>> = ({ children, ...props }) => {
     const [renderTime, setRenderTime] = useState(0);
 
@@ -37,7 +19,11 @@ const withProfiler = (WrappedComponent: React.ComponentType) => {
       }
     }, [renderTime]);
 
-    return <WrappedComponent {...props} />;
+    return (
+      <WrappedComponent {...props}>;
+        {children}
+      </WrappedComponent>
+    );
   };
 
   return ProfilerComponent;
